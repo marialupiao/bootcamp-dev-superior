61766ba751e3284d05b1eff0c6ee5cc6c3c7be63
Maria <marialupiao@outlook.com>
2023-08-14 19:41:46 -0300

    desafio-orm

---
+++ 1-java-spring-essential/desafio-modelo-dominio-ORM/src/main/java/com/devsuperior/desafiomodelodominioORM/entities/Atividade.java
@@ -0,0 +1,81 @@
+package com.devsuperior.desafiomodelodominioORM.entities;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "tb_atividade")
+public class Atividade {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nome;
+    private String descricao;
+    private Double preco;
+    @ManyToOne
+    @JoinColumn(name = "categoria_id")
+    private Categoria categoria;
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+    @OneToMany(mappedBy = "atividade")
+    private List<Participante> participantes = new ArrayList<>();
+
+    public List<Participante> getParticipantes() {
+        return participantes;
+    }
+    @OneToMany(mappedBy = "atividade")
+    private List<Bloco> blocos = new ArrayList<>();
+
+    public List<Bloco> getBlocos() {
+        return blocos;
+    }
+
+    public Atividade() { }
+
+    public Atividade(Integer id, String nome, String descricao, Double preco) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.preco = preco;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+}

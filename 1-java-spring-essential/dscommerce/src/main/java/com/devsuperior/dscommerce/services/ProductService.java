1508e7672f05e97225b98b895f5d05d100b280ad
Maria <marialupiao@outlook.com>
2023-08-17 16:52:57 -0300

    POST new product

--- 1-java-spring-essential/dscommerce/src/main/java/com/devsuperior/dscommerce/services/ProductService.java
+++ 1-java-spring-essential/dscommerce/src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -28,4 +28,16 @@ public class ProductService {
         return result.map(x -> new ProductDTO(x));
     }
 
+    @Transactional
+    public ProductDTO insert(ProductDTO productDTO) {
+        Product entity = new Product();
+        entity.setName(productDTO.getName());
+        entity.setDescription(productDTO.getDescription());
+        entity.setPrice(productDTO.getPrice());
+        entity.setImgUrl(productDTO.getImgUrl());
+
+        entity = productRepository.save(entity);
+        return new ProductDTO(entity);
+    }
+
 }
